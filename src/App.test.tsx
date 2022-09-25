f29601582577cda1889136fc2bfa83d97102b7ed
Alan Gorton <8091018+alangorton@users.noreply.github.com>
2022-09-25 12:45:12 +0100

    adds tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('<App />', () => {
+    beforeAll(() => {
+        jest.useFakeTimers();
+    });
+
+    afterAll(() => {
+        jest.useRealTimers();
+    });
+
+    it('shows current time initially', async () => {
+        jest.setSystemTime(new Date('1970-12-31T01:02:34'))
+
+        render(<App />);
+
+        expect(screen.getByText('01:02:34')).toBeInTheDocument();
+    });
+
+    it('updates time every second', async () => {
+        jest.setSystemTime(new Date('1970-12-31T23:59:59'))
+
+        render(<App />);
+
+        act(() => { jest.advanceTimersByTime(500) });
+        expect(await screen.findByText('23:59:59')).toBeInTheDocument();
+
+        act(() => { jest.advanceTimersByTime(500); });
+        expect(await screen.findByText('00:00:00')).toBeInTheDocument();
+    });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import dayjs from 'dayjs';
+import { useEffect, useState } from 'react';
+
+function getFormattedTime(): string {
+    return dayjs(Date.now()).format('HH:mm:ss');
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [currentTime, setCurrentTime] = useState(getFormattedTime());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setCurrentTime(getFormattedTime());
+        }, 1000);
+
+        return () => clearInterval(intervalId);
+    });
+
+    return <div>{currentTime}</div>;
 }
 
 export default App;
